a0efe240c4767fadcd6fd146093ec2c98cd071c6
cafeconlechee <magabriela0715@gmail.com>
2021-06-02 00:53:56 -0600

    Problema de ruta al Ver el codigo :c

--- app.py
+++ app.py
@@ -221,7 +221,7 @@ def delete_project():
 # Ruta para ver los proyectos en modo texto
 @app.route('/project/<username>/text_mode/<project_name>/', methods=['GET', 'POST'])
 def show_project(username, project_name):
-    project_path = path.join('.', 'project', username, 'text_mode', project_name)
+    project_path = path.join('.', 'project', username, 'text_mode', project_name, 'BIN')
     if request.method == 'POST':
         file = (request.get_json())['filename']
         file_ext = file.split('.')[-1] # Siempre va a elegir la ultima extensión, por si el nombre es name.something.c
@@ -301,7 +301,12 @@ def about():
 
 @app.route('/examples/intro')
 def text_mode():
-    return render_template("text_mode/text.html")
+    user = {}
+    if session.get('user_id'):
+        user_id = ObjectId(session.get('user_id'))
+        user = get_user_and_project(user_id)
+
+    return render_template("text_mode/text.html", user=user)
 
 @app.route('/logout')
 def logout():
